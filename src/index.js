1a3c990cf2b3350f76adc9bbffb0e4e7d0999eb1
pylkonata <78379183+pylkonata@users.noreply.github.com>
2021-03-18 18:29:51 +0200

    feat: add js code to index.js
    feat: add js code to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,67 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr) {
+  let minNum;
+  let result = 0;
+  
+  if (arr == undefined) return 0;
+  
+  if ((arr.length != undefined)&&(arr.length != 0))
+  {	
+      minNum = arr[0];
+    
+    for (let i = 0; i < arr.length; i++)
+    { 
+    //result = arr[i];
+    
+      if (arr[i] < minNum) minNum = arr[i];
+    //minNum = Math.min(minNum,result);
+    }
+  }
+  else
+  {
+    minNum = 0;
+    return minNum;
+  }
+  return minNum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr) {
+  let maxNum=0;
+  let result=0;
+
+  if (arr.length != undefined) 
+  {	
+    for (let i = 0; i < arr.length; i++) 
+    { 
+    result = arr[i];
+    maxNum = Math.max(maxNum,result);
+    }
+  }
+  else
+  {
+    maxNum = 0;
+  }
+
+
+  return maxNum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr) {
+  let avgNum=0;
+  let result=0;
+
+  if (arr.length != undefined) 
+  {	
+	  for (let i = 0; i < arr.length; i++)
+	  { 
+	  result  += arr[i];
+	
+	  }
+	  avgNum = (result/arr.length);
+  }
+  else
+  {
+	minNum = 0;
+  }
+ return avgNum;
 }
